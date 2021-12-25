0b6b885cbd82b5e493208bd97a90584421eb1780
Leonidas <leonidas.deimantas@gmail.com>
2021-12-25 22:47:50 +0200

    WEB: adding graphs initial

--- th-web/src/features/customComponents/SensorGraph.js
+++ th-web/src/features/customComponents/SensorGraph.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import {
-    selectMeasurements
-} from './sensorDataSlice';
+import { selectMeasurements } from '../sensorData/sensorDataSlice';
 import {
     AreaChart,
     Area,
@@ -22,39 +20,47 @@ export function SensorGraph(props) {
     }
 
     return (
-        <div className="flex flex-col col-span-full sm:col-span-6 bg-white shadow-lg rounded-sm border border-gray-200">
-            <header className="px-5 py-4 border-b border-gray-100">
-                <h2 className="font-semibold text-gray-800">{props.place}</h2>
-            </header>
-            <div className="px-5 pt-5">
-                <div className="text-xs font-semibold text-gray-400 uppercase mb-1">{props.desc}</div>
-                <div className="flex items-start">
-                    <div className="text-3xl font-bold text-gray-800 mr-2">{lastItem}{props.unit}</div>
+        <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded">
+        <div className="rounded-t mb-0 px-4 py-3 bg-transparent">
+            <div className="flex flex-wrap items-center">
+                <div className="relative w-full max-w-full flex-grow flex-1">
+                    <h6 className="uppercase text-blueGray-400 mb-1 text-xs font-semibold">
+                        {props.place}
+                    </h6>
+                    <h2 className="text-blueGray-700 text-xl font-semibold">
+                        {props.desc}
+                    </h2>
                 </div>
             </div>
+        </div>
+            <div className="p-4 flex-auto">
+                {/* Chart */}
+                <div className="relative h-300-px">
 
-            <ResponsiveContainer width={"100%"} height={250}>
-                <AreaChart data={values} margin={{ top: 15, right: 30, left: 30, bottom: 15 }}>
-                    <defs>
-                        <linearGradient id={props.data} x1="0" y1="0" x2="0" y2="1">
-                            <stop offset="5%" stopColor={props.color} stopOpacity={0.8} />
-                            <stop offset="95%" stopColor={props.color} stopOpacity={0} />
-                        </linearGradient>
-                    </defs>
-                    <XAxis hide={true} axisLine={false} tickLine={false} type="number" dataKey="time" domain={["dataMin", "dataMax"]} tickFormatter={date => moment(date).format('HH:mm')} />
-                    <YAxis hide={true} domain={["dataMin - 3", "dataMax + 3"]} />
-                    <Tooltip labelFormatter={date => "Time: " + moment(date).format('HH:mm')} formatter={(okay) => [new Intl.NumberFormat('en').format(okay), undefined]} />
-                    <Area
-                        type="monotone"
-                        dataKey={props.data}
-                        stroke={props.color}
-                        name={props.desc}
-                        unit={props.unit}
-                        fillOpacity={1}
-                        fill={"url(#" + props.data + ")"}
-                    />
-                </AreaChart>
-            </ResponsiveContainer>
+                    <ResponsiveContainer width={"100%"} height={300}>
+                        <AreaChart data={values} margin={{ top: 15, right: 30, left: 30, bottom: 15 }}>
+                            <defs>
+                                <linearGradient id={props.data} x1="0" y1="0" x2="0" y2="1">
+                                    <stop offset="5%" stopColor={props.color} stopOpacity={0.8} />
+                                    <stop offset="95%" stopColor={props.color} stopOpacity={0} />
+                                </linearGradient>
+                            </defs>
+                            <XAxis hide={true} axisLine={false} tickLine={false} type="number" dataKey="time" domain={["dataMin", "dataMax"]} tickFormatter={date => moment(date).format('HH:mm')} />
+                            <YAxis hide={true} domain={["dataMin - 3", "dataMax + 3"]} />
+                            <Tooltip labelFormatter={date => "Time: " + moment(date).format('HH:mm')} formatter={(okay) => [new Intl.NumberFormat('en').format(okay), undefined]} />
+                            <Area
+                                type="monotone"
+                                dataKey={props.data}
+                                stroke={props.color}
+                                name={props.desc}
+                                unit={props.unit}
+                                fillOpacity={1}
+                                fill={"url(#" + props.data + ")"}
+                            />
+                        </AreaChart>
+                    </ResponsiveContainer>
+                </div>
+            </div>
         </div>
 
     );
@@ -64,50 +70,61 @@ export function SensorGraphDouble(props) {
     const values = useSelector(state => selectMeasurements(state, props.sensorId));
 
     return (
-        <div className="flex flex-col col-span-full sm:col-span-6 bg-white shadow-lg rounded-sm border border-gray-200">
-            <header className="px-5 py-4 border-b border-gray-100">
-                <h2 className="font-semibold text-gray-800">{props.place}</h2>
-            </header>
+        <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded bg-blueGray-700">
+            <div className="rounded-t mb-0 px-4 py-3 bg-transparent">
+                <div className="flex flex-wrap items-center">
+                    <div className="relative w-full max-w-full flex-grow flex-1">
+                        <h6 className="uppercase text-blueGray-100 mb-1 text-xs font-semibold">
+                            {props.place}
+                        </h6>
+                    </div>
+                </div>
+            </div>
+            <div className="p-4 flex-auto">
+                {/* Chart */}
+                <div className="relative h-350-px">
 
-            <ResponsiveContainer width={"100%"} height={400}>
-                <AreaChart data={values} margin={{ top: 15, right: 30, left: 30, bottom: 15 }}>
-                    <defs>
-                        <linearGradient id={props.data1} x1="0" y1="0" x2="0" y2="1">
-                            <stop offset="5%" stopColor={props.color1} stopOpacity={0.8} />
-                            <stop offset="95%" stopColor={props.color1} stopOpacity={0} />
-                        </linearGradient>
-                        <linearGradient id={props.data2} x1="0" y1="0" x2="0" y2="1">
-                            <stop offset="5%" stopColor={props.color2} stopOpacity={0.8} />
-                            <stop offset="95%" stopColor={props.color2} stopOpacity={0} />
-                        </linearGradient>
-                    </defs>
-                    <XAxis hide={true} axisLine={false} tickLine={false} type="number" dataKey="time" domain={["dataMin", "dataMax"]} tickFormatter={date => moment(date).format('HH:mm')} />
-                    <YAxis yAxisId={props.data1} hide={true} axisLine={false} tickLine={false} domain={["dataMin - 3", "dataMax + 3"]} />
-                    <YAxis yAxisId={props.data2} hide={true} axisLine={false} tickLine={false} domain={["dataMin - 3", "dataMax + 3"]} />
-                    <Tooltip labelFormatter={date => "Time: " + moment(date).format('HH:mm')} formatter={(okay) => [new Intl.NumberFormat('en').format(okay), undefined]} />
-                    <Legend verticalAlign="top" height={36} />
-                    <Area
-                        type="monotone"
-                        yAxisId={props.data1}
-                        dataKey={props.data1}
-                        stroke={props.color1}
-                        name={props.desc1}
-                        unit={props.unit1}
-                        fillOpacity={1}
-                        fill={"url(#" + props.data1 + ")"}
-                    />
-                    <Area
-                        type="monotone"
-                        yAxisId={props.data2}
-                        dataKey={props.data2}
-                        stroke={props.color2}
-                        name={props.desc2}
-                        unit={props.unit2}
-                        fillOpacity={1}
-                        fill={"url(#" + props.data2 + ")"}
-                    />
-                </AreaChart>
-            </ResponsiveContainer>
+                    <ResponsiveContainer width={"100%"} height={350}>
+                        <AreaChart data={values} margin={{ top: 15, right: 30, left: 30, bottom: 15 }}>
+                            <defs>
+                                <linearGradient id={props.data1} x1="0" y1="0" x2="0" y2="1">
+                                    <stop offset="5%" stopColor={props.color1} stopOpacity={0.8} />
+                                    <stop offset="95%" stopColor={props.color1} stopOpacity={0} />
+                                </linearGradient>
+                                <linearGradient id={props.data2} x1="0" y1="0" x2="0" y2="1">
+                                    <stop offset="5%" stopColor={props.color2} stopOpacity={0.8} />
+                                    <stop offset="95%" stopColor={props.color2} stopOpacity={0} />
+                                </linearGradient>
+                            </defs>
+                            <XAxis hide={true} axisLine={false} tickLine={false} type="number" dataKey="time" domain={["dataMin", "dataMax"]} tickFormatter={date => moment(date).format('HH:mm')} />
+                            <YAxis yAxisId={props.data1} hide={true} axisLine={false} tickLine={false} domain={["dataMin - 3", "dataMax + 3"]} />
+                            <YAxis yAxisId={props.data2} hide={true} axisLine={false} tickLine={false} domain={["dataMin - 3", "dataMax + 3"]} />
+                            <Tooltip labelFormatter={date => "Time: " + moment(date).format('HH:mm')} formatter={(okay) => [new Intl.NumberFormat('en').format(okay), undefined]} />
+                            <Legend verticalAlign="top" height={36} />
+                            <Area
+                                type="monotone"
+                                yAxisId={props.data1}
+                                dataKey={props.data1}
+                                stroke={props.color1}
+                                name={props.desc1}
+                                unit={props.unit1}
+                                fillOpacity={1}
+                                fill={"url(#" + props.data1 + ")"}
+                            />
+                            <Area
+                                type="monotone"
+                                yAxisId={props.data2}
+                                dataKey={props.data2}
+                                stroke={props.color2}
+                                name={props.desc2}
+                                unit={props.unit2}
+                                fillOpacity={1}
+                                fill={"url(#" + props.data2 + ")"}
+                            />
+                        </AreaChart>
+                    </ResponsiveContainer>
+                </div>
+            </div>
         </div>
 
     );
--- th-web/src/views/admin/Dashboard copy.js
+++ th-web/src/views/admin/Dashboard copy.js
@@ -6,18 +6,24 @@ import CardLineChart from "components/Cards/CardLineChart.js";
 import CardBarChart from "components/Cards/CardBarChart.js";
 import CardPageVisits from "components/Cards/CardPageVisits.js";
 import CardSocialTraffic from "components/Cards/CardSocialTraffic.js";
-import { SensorGraph, SensorGraphDouble } from "features/customComponents/SensorGraph";
 
 export default function Dashboard() {
     return (
         <>
             <div className="flex flex-wrap">
                 <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
-                    <SensorGraph place={"Darbo kambarys"} data={"temperature"} desc={"Temperature"} unit={"°C"} color={"#82ca9d"} sensorId={"1234567891"} />
+                    <CardLineChart />
                 </div>
                 <div className="w-full xl:w-4/12 px-4">
-                <SensorGraphDouble place={"Darbo kambarys"} sensorId={"1234567893"} data1={"temperature"} desc1={"Temperature"} unit1={"°C"} color1={"#82ca9d"}
-                                                                        data2={"humidity"} desc2={"Humidity"} unit2={"%"} color2={"#8884d8"}/>
+                    <CardBarChart />
+                </div>
+            </div>
+            <div className="flex flex-wrap mt-4">
+                <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
+                    <CardPageVisits />
+                </div>
+                <div className="w-full xl:w-4/12 px-4">
+                    <CardSocialTraffic />
                 </div>
             </div>
         </>
